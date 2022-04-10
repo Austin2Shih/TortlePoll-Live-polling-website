a08427bcdf0affe966fe2f9a07e3ed31f497218e
Austin Shih <austinhshih@gmail.com>
2022-04-10 11:04:49 -0700

    Fixed env vars

--- pages/api/handle_click.js
+++ pages/api/handle_click.js
@@ -2,17 +2,17 @@ import { ObjectID } from 'bson';
 import clientPromise from '../../util/mongodb';
 const Pusher = require('pusher')
 
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+  useTLS: true,
+})
+
 export default async function handler(req, res) {
   const client = await clientPromise
   const db = client.db(process.env.MONGODB_DB)
-
-  const pusher = new Pusher({
-    appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID,
-    key: process.env.NEXT_PUBLIC_PUSHER_KEY,
-    secret: process.env.NEXT_PUBLIC_PUSHER_SECRET,
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-    useTLS: true,
-  })
 
   const response = await db.collection("button_clicks").updateOne(
     {
