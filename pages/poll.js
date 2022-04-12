0f65786eef44382b2d4551a9ae907c7a2fea9ef9
Austin Shih <austinhshih@gmail.com>
2022-04-11 22:59:56 -0700

    Socket stuff

--- pages/poll.js
+++ pages/poll.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { ObjectID } from 'bson';
 import clientPromise from '../util/mongodb'
 import PollDisplay from '../components/PollDisplay';
+import io from 'Socket.IO-client'
 
 // Initializing Pusher
 var pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
@@ -12,6 +13,8 @@ var pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
 
 // Variable to check if binded to Pusher
 var bound = false
+
+let socket
 
 // Subscribing to pusher channel
 const channel = pusher.subscribe('polling-development')
@@ -42,26 +45,55 @@ export default function Poll(props) {
     
     const pollID = props.data._id
 
-    useEffect(() => {
-        if (!bound) {
-            channel.bind(`new-vote-${pollID}`, async () => {
-                await fetch(`/api/get_votes`, {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        "_id" : `${pollID}`,
-                    }),
-                    headers: {
-                        "Content-type": "application/json; charset=UTF-8"
-                    }
-                }).then(async (response) => {
-                    await response.json().then((res) => {
-                        setPollData(<PollDisplay data={res}></PollDisplay>)
-                    })
+    useEffect(() => socketInitializer(), [])
+
+    const socketInitializer = () => {
+        socket = io()
+
+        socket.on('connect', () => {
+            console.log('connected')
+        })
+
+        socket.on(`new-vote`, async () => {
+            await fetch(`/api/get_votes`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    "_id" : `${pollID}`,
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            }).then(async (response) => {
+                await response.json().then((res) => {
+                    setPollData(<PollDisplay data={res}></PollDisplay>)
                 })
-            })          
-            bound = true
-        }
-    }, [])
+            })
+        })  
+            
+        
+    }
+
+
+    // useEffect(() => {
+    //     if (!bound) {
+    //         channel.bind(`new-vote-${pollID}`, async () => {
+    //             await fetch(`/api/get_votes`, {
+    //                 method: 'POST',
+    //                 body: JSON.stringify({
+    //                     "_id" : `${pollID}`,
+    //                 }),
+    //                 headers: {
+    //                     "Content-type": "application/json; charset=UTF-8"
+    //                 }
+    //             }).then(async (response) => {
+    //                 await response.json().then((res) => {
+    //                     setPollData(<PollDisplay data={res}></PollDisplay>)
+    //                 })
+    //             })
+    //         })          
+    //         bound = true
+    //     }
+    // }, [])
     
     return (
         <div>
