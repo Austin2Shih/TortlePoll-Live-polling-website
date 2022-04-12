a0ea93f8eb8881760633c3de9545cf512ef5b0ae
Austin Shih <austinhshih@gmail.com>
2022-04-11 23:00:10 -0700

    Revert "Socket stuff"
    This reverts commit 0f65786eef44382b2d4551a9ae907c7a2fea9ef9.

--- pages/api/handle_vote.js
+++ pages/api/handle_vote.js
@@ -1,20 +1,24 @@
 import { ObjectID } from 'bson';
 import clientPromise from '../../util/mongodb';
-import pusher from '../../util/pusher';
-import io from 'Socket.IO-client'
+const Pusher = require('pusher')
 
 export default async function handler(req, res) {
   const client = await clientPromise
   const db = client.db(process.env.MONGODB_POLLS)
+
+  let pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+    cluster: process.env.PUSHER_CLUSTER,
+    useTLS: true,
+  })
 
   const data = req.body
   const pollID = data._id
   const voteIndex = data.index
 
   console.log("handled vote")
-
-  let socket = io()
-  
 
   const response = await db.collection("polls").updateOne(
     {
@@ -28,10 +32,9 @@ export default async function handler(req, res) {
       upsert: true
     }
   ).then(() => {
-    // pusher.trigger('polling-development', `new-vote-${pollID}`, {}, () => {
-    //   res.status(200).end('vote sent successfully')
-    // })
-    socket.broadcast.emit('new-vote', "bleh")
+    pusher.trigger('polling-development', `new-vote-${pollID}`, {}, () => {
+      res.status(200).end('vote sent successfully')
+    })
   })
 
   res.json(response);
--- pages/poll.js
+++ pages/poll.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import { ObjectID } from 'bson';
 import clientPromise from '../util/mongodb'
 import PollDisplay from '../components/PollDisplay';
-import io from 'Socket.IO-client'
 
 // Initializing Pusher
 var pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
@@ -13,8 +12,6 @@ var pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
 
 // Variable to check if binded to Pusher
 var bound = false
-
-let socket
 
 // Subscribing to pusher channel
 const channel = pusher.subscribe('polling-development')
@@ -45,55 +42,26 @@ export default function Poll(props) {
     
     const pollID = props.data._id
 
-    useEffect(() => socketInitializer(), [])
-
-    const socketInitializer = () => {
-        socket = io()
-
-        socket.on('connect', () => {
-            console.log('connected')
-        })
-
-        socket.on(`new-vote`, async () => {
-            await fetch(`/api/get_votes`, {
-                method: 'POST',
-                body: JSON.stringify({
-                    "_id" : `${pollID}`,
-                }),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            }).then(async (response) => {
-                await response.json().then((res) => {
-                    setPollData(<PollDisplay data={res}></PollDisplay>)
+    useEffect(() => {
+        if (!bound) {
+            channel.bind(`new-vote-${pollID}`, async () => {
+                await fetch(`/api/get_votes`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        "_id" : `${pollID}`,
+                    }),
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                }).then(async (response) => {
+                    await response.json().then((res) => {
+                        setPollData(<PollDisplay data={res}></PollDisplay>)
+                    })
                 })
-            })
-        })  
-            
-        
-    }
-
-
-    // useEffect(() => {
-    //     if (!bound) {
-    //         channel.bind(`new-vote-${pollID}`, async () => {
-    //             await fetch(`/api/get_votes`, {
-    //                 method: 'POST',
-    //                 body: JSON.stringify({
-    //                     "_id" : `${pollID}`,
-    //                 }),
-    //                 headers: {
-    //                     "Content-type": "application/json; charset=UTF-8"
-    //                 }
-    //             }).then(async (response) => {
-    //                 await response.json().then((res) => {
-    //                     setPollData(<PollDisplay data={res}></PollDisplay>)
-    //                 })
-    //             })
-    //         })          
-    //         bound = true
-    //     }
-    // }, [])
+            })          
+            bound = true
+        }
+    }, [])
     
     return (
         <div>
