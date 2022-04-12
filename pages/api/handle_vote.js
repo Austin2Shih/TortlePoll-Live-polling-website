a675562546fc6924b9473e664c5d2cf78ab43490
Austin Shih <austinhshih@gmail.com>
2022-04-11 23:06:32 -0700

    Back to before

--- pages/api/handle_vote.js
+++ pages/api/handle_vote.js
@@ -1,25 +1,15 @@
 import { ObjectID } from 'bson';
 import clientPromise from '../../util/mongodb';
-const Pusher = require('pusher')
+import pusher from '../../util/pusher';
 
 export default async function handler(req, res) {
   const client = await clientPromise
   const db = client.db(process.env.MONGODB_POLLS)
 
-  let pusher = new Pusher({
-    appId: process.env.PUSHER_APP_ID,
-    key: process.env.PUSHER_KEY,
-    secret: process.env.PUSHER_SECRET,
-    cluster: process.env.PUSHER_CLUSTER,
-    useTLS: true,
-  })
-
   const data = req.body
   const pollID = data._id
   const voteIndex = data.index
-
-  console.log("handled vote")
-
+  
   const response = await db.collection("polls").updateOne(
     {
       "_id": ObjectID(pollID),
@@ -32,9 +22,7 @@ export default async function handler(req, res) {
       upsert: true
     }
   ).then(() => {
-    pusher.trigger('polling-development', `new-vote-${pollID}`, {}, () => {
-      res.status(200).end('vote sent successfully')
-    })
+    pusher.trigger('polling-development', `new-vote-${pollID}`, {})
   })
 
   res.json(response);
