4624ef14c33cf6f95606ba0c5469548a2efd2472
Austin Shih <austinhshih@gmail.com>
2022-04-13 15:51:42 -0700

    Removed triple trigger

--- pages/api/pusher-trigger.js
+++ pages/api/pusher-trigger.js
@@ -8,24 +8,10 @@ let pusher = new Pusher({
   keepAlive: true
 })
 
-let test = new Pusher({
-    appId: process.env.PUSHER_APP_ID,
-    key: process.env.PUSHER_KEY,
-    secret: process.env.PUSHER_SECRET,
-    cluster: process.env.PUSHER_CLUSTER,
-    keepAlive: true
-  })
-
 module.exports = (req, res) => {
     const data = req.body
     const pollID = data._id
-    pusher.trigger('polling-development', `new-vote-${pollID}`, data, () => {
-        res.status(200).end('sent event successfully');
-    });
-
-    test.trigger('polling-development', `new-vote-${pollID}`, data, () => {
-        res.status(200).end('sent event successfully');
-    });
+    pusher.trigger('polling-development', `new-vote-${pollID}`);
 
     res.json(data)
 };
