7829b68d5f56d90c6d34599efd69aef39c51e934
Austin Shih <austinhshih@gmail.com>
2022-04-22 19:25:47 -0700

    Updated Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,28 @@ import styles from '../styles/Navbar.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../public/cowboy_turtle.png'
+import { useUser } from '../util/auth/useUser'
+import { useState, useEffect } from 'react'; 
+
+
 export default function Navbar() {
+    const {user, logout} = useUser();
+    const [loginButton, setLoginButton] = useState(<Link href="/login">Log in</Link>);
+
+    useEffect(() => {
+      if (user) {
+          setLoginButton(<Link href="/" onClick={() => {logout()}}>Log out</Link>)
+      } else {
+          setLoginButton(<Link href="/login">Log in</Link>)
+      }
+    }, [user])
+
     return (
         <div className={styles.container}>
             <ul className={styles.list}>
+                <div className={styles.logoContainer}>
+                    <Image src={logo}></Image>
+                </div>
                 <li className={styles.listItem}>
                     <Link href="/create_poll">Create poll</Link>
                 </li>
@@ -13,13 +31,15 @@ export default function Navbar() {
                     <Link href="/dashboard">Broswe polls</Link>
                 </li>
             </ul>
-            <ul className={styles.list}>
+            <ul className={styles.rightList}>
                 <li className={styles.listItem}>
-                    <Link href="/login">Log in</Link>
+                    {loginButton}
                 </li>
                 <li className={styles.listItem}>
                     <button className={styles.button}>
-                        <Link href="/signup">Sign up</Link>
+                        <Link href="/signup">
+                            <a className={styles.buttonText}>Sign up</a>
+                        </Link>
                     </button>
                 </li>
             </ul>
